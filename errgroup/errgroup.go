9ea5e09cad4aa9d9f7f9c99ca47a2df1e249eb5b
yuxi-o <88wangqh@163.com>
2021-08-02 01:40:21 +0800

    [concurrency] errgroup support http and signal exit at the same time

--- errgroup/errgroup.go
+++ errgroup/errgroup.go
@@ -1,56 +1,83 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"golang.org/x/sync/errgroup"
 )
 
-func HandleSignal() error {
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
+var errorGroup *errgroup.Group
+var errorCtx context.Context
+var httpServer *http.Server
+var signalChan chan os.Signal = make(chan os.Signal, 10)
 
-	for sg := range sc {
+func pingHandle(w http.ResponseWriter, res *http.Request) {
+	fmt.Fprintf(w, "{\"msg\": \"pong\"}")
+}
+
+func getBaseContext(net.Listener) context.Context {
+	return errorCtx
+}
+
+func HandleHttp() error {
+	if err := httpServer.ListenAndServe(); err != nil {
+		signalChan <- syscall.SIGTERM
+		return fmt.Errorf("[httpServer] ListenAndServe error: %v", err)
+	}
+	return nil
+}
+
+func HandleSignal() error {
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR1, syscall.SIGUSR2)
+	//signal.Notify(sc)
+
+	for sg := range signalChan {
 		switch sg {
 		case syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM:
 			return fmt.Errorf("[signal] exit from signal(%v)", sg)
 		case syscall.SIGUSR1:
-			fmt.Println("[signal] deal with signal ", sg)
+			fmt.Printf("[signal] deal with signal(%v)\n", sg)
 		case syscall.SIGUSR2:
-			fmt.Println("[signal] deal with signal ", sg)
+			if err := httpServer.Shutdown(errorCtx); err != nil {
+				return fmt.Errorf("[signal] shutdown httpServer error: %v", err)
+			}
+			return fmt.Errorf("[signal] receive signal(%v) and shutdown http server", sg)
 		default:
-			fmt.Println("[signal] Unknown signal", sg)
+			fmt.Printf("[signal] Unknown signal(%v)\n", sg)
 		}
 	}
 
 	return nil
 }
 
-func HandleHttp() error {
-	http.HandleFunc("/ping", func(w http.ResponseWriter, res *http.Request) {
-		fmt.Fprintf(w, "{\"msg\": \"pong\"}")
-	})
-
-	if err := http.ListenAndServe(":9090", nil); err != nil {
-		return err
-	}
-	return nil
-}
-
 func main() {
-	//	eg, ctx := errgroup.WithContext(context.Background())
-	eg := new(errgroup.Group)
+	errorGroup, errorCtx = errgroup.WithContext(context.Background())
+	//eg := new(errgroup.Group)
 
-	eg.Go(HandleSignal)
+	httpServeMux := http.NewServeMux()
+	httpServeMux.HandleFunc("/ping", pingHandle)
 
-	eg.Go(HandleHttp)
+	httpServer = &http.Server{
+		Addr:           ":8088",
+		Handler:        httpServeMux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+		BaseContext:    getBaseContext,
+	}
 
-	if err := eg.Wait(); err != nil {
+	errorGroup.Go(HandleSignal)
+	errorGroup.Go(HandleHttp)
+
+	if err := errorGroup.Wait(); err != nil {
 		log.Printf("[main] exit: %v", err)
 	}
 }
